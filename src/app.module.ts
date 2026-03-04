1373cd8b0d25ba0e64299434cd69462c7e163a55
yusufkaan-dev <yusufesra1303@gmail.com>
2026-03-05 02:19:42 +0300

    Fix app.module ParentApp wiring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ParentAppModule } from './parent-app/parent-app.module';
+
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { DriverApiModule } from './driver-api/driver-api.module';
+import { DriversModule } from './drivers/drivers.module';
 import { InstitutionsModule } from './institutions/institutions.module';
 import { ParentsModule } from './parents/parents.module';
 import { StudentsModule } from './students/students.module';
-import { DriversModule } from './drivers/drivers.module';
-import { LocationModule } from './location/location.module';
+import { DriverApiModule } from './driver-api/driver-api.module';
 import { RoutesModule } from './routes/routes.module';
 import { DailyRoutesModule } from './daily-routes/daily-routes.module';
-import { ParentAppController } from './parent-app.controller';
+import { LocationModule } from './location/location.module';
+
+import { ParentAppModule } from './parent-app/parent-app.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [ParentAppController],
-})
-export class ParentAppModule {}
-@Module({
- imports: [
-  // ... diğer modüller
-  LocationModule,
-  ParentAppModule,
+  imports: [
     PrismaModule,
     AuthModule,
     UsersModule,
@@ -33,10 +26,10 @@ export class ParentAppModule {}
     ParentsModule,
     StudentsModule,
     DriverApiModule,
-
-    // NEW
     RoutesModule,
     DailyRoutesModule,
+    LocationModule,
+    ParentAppModule,
   ],
   controllers: [AppController],
   providers: [AppService],
